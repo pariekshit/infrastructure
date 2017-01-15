f68578cf952ae3b2f9f6ec3dc5535b4219287981
root <root@ip-172-31-21-107.us-east-2.compute.internal>
2017-01-15 19:29:11 +0000

    added terminate file

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-#ansible-playbook -i inventory/hosts -s provision_instance.yml  -e "env=dev"
-
-ansible-playbook -i inventory/hosts -s setup_instance.yml  -e env=dev -v --private-key=/opt/pari/test.pem
-
-ansible-playbook -i inventory/hosts -s deploy_app.yml --extra-vars "env=dev artifact=/var/lib/jenkins/workspace/test/target/CounterWebApp.war" -v --private-key=/opt/pari/test.pem
